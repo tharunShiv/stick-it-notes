258a66e41404c5ad915861bb423ba7695150f165
tharunshiv <tharun.rocky14@gmail.com>
2020-04-22 15:03:23 +0530

    local storage implemented

--- myscripts.js
+++ myscripts.js
@@ -2,33 +2,23 @@ var itemList = document.getElementById("notes");
 
 itemList.addEventListener("click", removeItem);
 
-let createNote = () => {
+let count = Number(window.localStorage.getItem("count"));
+if (!count) {
+  window.localStorage.setItem("count", "0");
+}
+
+console.log(count);
+
+let createNote = (noteTitle, noteBody) => {
+  if (count > 0) {
+    document.getElementById("no-notes").className = "hidden";
+  }
+
   var li = document.createElement("li");
   var a = document.createElement("a");
   var h2 = document.createElement("h2");
   var p = document.createElement("p");
   var ul = document.getElementById("notes");
-
-  var noteTitle = document.getElementById("new-note-title-input").value;
-  var noteBody = document.getElementById("new-note-body-input").value;
-
-  let count = Number(window.localStorage.getItem("count"));
-  if (!count) {
-    window.localStorage.setItem("count", "0");
-  }
-
-  window.localStorage.setItem(noteTitle, noteBody);
-  count += 1;
-  window.localStorage.setItem("count", count);
-
-  for (i = 0; i < count; i++) {
-    console.log(window.localStorage.key(i));
-  }
-
-  if (!noteTitle || !noteBody) {
-    alert("The note Title and Body are necessary");
-    return;
-  }
 
   let xButton = document.createElement("button");
   xButton.classList.add("delete");
@@ -41,12 +31,35 @@ let createNote = () => {
   xButton.appendChild(xText);
 
   a.appendChild(h2);
-  a.appendChild(p);
   a.appendChild(xButton);
+  a.appendChild(p);
   a.setAttribute("href", "#");
 
   li.appendChild(a);
   ul.appendChild(li);
+};
+
+let createNoteFromInput = () => {
+  var noteTitle = document.getElementById("new-note-title-input").value;
+  var noteBody = document.getElementById("new-note-body-input").value;
+
+  document.getElementById("new-note-title-input").value = "";
+  document.getElementById("new-note-body-input").value = "";
+
+  console.log("yes");
+  if (!noteTitle || !noteBody) {
+    alert("Both Title and body of the note must be provided");
+    return;
+  }
+  count += 1;
+  window.localStorage.setItem("count", count);
+
+  while (window.localStorage.getItem(noteTitle)) {
+    noteTitle = noteTitle + " - 1";
+  }
+  window.localStorage.setItem(noteTitle, noteBody);
+
+  createNote(noteTitle, noteBody);
 };
 
 function removeItem(e) {
@@ -55,9 +68,9 @@ function removeItem(e) {
     console.log(e);
     if (
       confirm(
-        "Are you sure to delete the " +
+        'Are you sure to delete the "' +
           e.target.previousElementSibling.innerText +
-          " note?"
+          '" note?'
       )
     ) {
       //grab the parent
@@ -65,6 +78,21 @@ function removeItem(e) {
       var li = e.target.parentElement.parentElement;
 
       itemList.removeChild(li);
+      count -= 1;
+      window.localStorage.setItem("count", count);
+      window.localStorage.removeItem(e.target.previousElementSibling.innerText);
+      if (count < 1) {
+        document.getElementById("no-notes").className = "";
+      }
     }
+  }
+}
+
+for (i = 0; i < count + 1; i++) {
+  console.log(window.localStorage.key(i));
+  let noteTitle = window.localStorage.key(i);
+  let noteBody = window.localStorage.getItem(noteTitle);
+  if (noteTitle !== "count") {
+    createNote(noteTitle, noteBody);
   }
 }
